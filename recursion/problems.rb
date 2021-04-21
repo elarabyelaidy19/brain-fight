7fc0063e97bb34407921384bc79df0e4f9475748
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-21 16:44:54 +0200

    add_numbers

--- recursion/problems.rb
+++ recursion/problems.rb
@@ -9,3 +9,14 @@ end
 p sum_to(9) 
 
 
+# Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers. Write this method recursively. 
+
+def add_numbers(nums_array) 
+
+  return nums_array.first if nums_array.length <= 1
+
+  nums_array.pop + add_numbers(nums_array) 
+
+end
+
+p add_numbers([3, 8, 8, 9, 2, 1, 6]) 
