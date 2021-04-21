ab2907899726fc6f5a994b05ffdae393003b7fbe
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-21 20:20:36 +0200

    ice cream shop

--- recursion/problems.rb
+++ recursion/problems.rb
@@ -37,3 +37,37 @@ p  gamma_fnc(1)  # => returns 1
 p  gamma_fnc(4)  # => returns 6
 p  gamma_fnc(8)  # => returns 5040
 
+
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop,
+# as well as the user's favorite ice cream flavor.Recursively find out whether or not the shop offers their favorite flavor. 
+
+def ice_cream_shop(flavors, favorite) 
+
+  return false if flavors.empty? 
+  return true if flavors.last == favorite 
+
+  ice_cream_shop(flavors[0...-1], favorite) 
+end 
+
+
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
+
+# Write a function reverse(string) that takes in a string and returns it reversed.
+
+def reverse(string) 
+
+  return '' if string.empty?
+
+  reverse(string[1..-1]) + string[0] 
+end
+
+p  reverse("house") # => "esuoh"
+p  reverse("dog") # => "god"
+p  reverse("atom") # => "mota"
+p  reverse("q") # => "q"
+p  reverse("id") # => "di"
+p  reverse("") # => ""
