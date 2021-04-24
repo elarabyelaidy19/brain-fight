d77db781da02124034c1e7d5b08f004b2db7c544
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-24 22:06:18 +0200

    loop

--- AppAcademy_Ex/loop.rb
+++ AppAcademy_Ex/loop.rb
@@ -35,3 +35,21 @@ end
 
 p count_e("eleare")  
 p  count_e "alaraby"
+
+
+# Write a method count_a(word) that takes in a string word and returns the number of a's in the word. The method should count both lowercase (a) and uppercase (A) 
+
+def count_a(word) 
+
+  count = 0 
+
+  (0...word.length).each do |ele| 
+     count += 1 if word[ele] == "a"  || word[ele] == "A" 
+  end 
+  count 
+end 
+
+puts count_a("application")  # => 2
+puts count_a("bike")         # => 0
+puts count_a("Arthur")       # => 1
+puts count_a("Aardvark")     # => 3 
