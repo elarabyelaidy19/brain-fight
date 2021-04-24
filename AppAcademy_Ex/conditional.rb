7a4e27da65feae640a0d68e5b48739fb7bd9538f
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-24 20:52:34 +0200

    conditions

--- AppAcademy_Ex/conditional.rb
+++ AppAcademy_Ex/conditional.rb
@@ -9,4 +9,23 @@ def is_div_by_5?(number)
 end 
 
 p is_div_by_5?(25) 
-p is_div_by_5?(23)
+p is_div_by_5?(23) 
+
+# Write a method either_only(number) that takes in a number and returns true if the number is divisible by either 3 or 5, but not both. The method should return false otherwise.
+
+def either_only?(number)  
+  
+  if number % 5 == 0 && number % 3 != 0 
+    return true  
+  end 
+  
+  if number % 5 != 0 && number % 3 == 0 
+    return true  
+  end 
+
+  false 
+end 
+
+p either_only?(15) 
+p either_only?(10) 
+p either_only?(9) 
