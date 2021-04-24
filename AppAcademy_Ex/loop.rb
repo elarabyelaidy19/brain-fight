9b703f80167ae49e37b9ecf35951bac0f6c6a9f6
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-24 22:13:57 +0200

    sum numbers

--- AppAcademy_Ex/loop.rb
+++ AppAcademy_Ex/loop.rb
@@ -77,6 +77,7 @@ puts count_vowels("pizza")     # => 2
 
 # Write a method sum_nums(max) that takes in a number max and returns the sum of all numbers from 1 up to and including max.  
 
+
 def sum_nums(number) 
 
   sum = 0 
@@ -88,4 +89,4 @@ def sum_nums(number)
 end 
 
 puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
-puts sum_nums(5) # => 15
+puts sum_nums(5) # => 15 
