e72b297adf4fea7ce77859cc5abcad94f6378ac1
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-25 21:01:49 +0200

    elemnt times index

--- AppAcademy_Ex/array.rb
+++ AppAcademy_Ex/array.rb
@@ -56,4 +56,23 @@ end
 
 p yell(["hello", "world"]) 
 puts
-p yell(["code", "is", "cool"]) 
+p yell(["code", "is", "cool"])  
+
+
+# Write a method element_times_index(nums) that takes in an array of numbers
+# and returns a new array containing every number of the original array multiplied with its index. 
+
+def element_times_index(nums) 
+  
+  new_array = [] 
+
+  (0...nums.length).each do |ele| 
+    new_array << nums[ele]*ele  
+  end 
+
+  new_array
+end 
+
+print element_times_index([4, 7, 6, 5])      
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) 
