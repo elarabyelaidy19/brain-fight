c01657bebe8b2940ad9738a49bd9419828bcc4c4
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-26 22:26:23 +0200

    count occurrences

--- recursion/more_exercise.rb
+++ recursion/more_exercise.rb
@@ -23,3 +23,17 @@ end
 p include?([1, 3, 3, 9], 5)
 p include?([], 5)
 p include?([5], 5)
+
+
+# Problem 3: You have an unsorted array of integers. Write a recursive solution to count the number of occurrences of a specific value. 
+
+def count_occurrences(array, value) 
+  return 0 if array.empty? 
+
+  count = array.first == value ? 1 : 0 
+  count + count_occurrences(array[1..-1], value) 
+end 
+
+p count_occurrences([1, 3, 3, 9], 3)
+
+
