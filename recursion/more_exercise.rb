6e8d53283279cab09dd4cdd787654785b04e6f9a
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-26 21:27:17 +0200

    include? element

--- recursion/more_exercise.rb
+++ recursion/more_exercise.rb
@@ -8,3 +8,18 @@ end
 
 
 p sum([1, 3, 5, 9]) 
+
+
+# Problem 2: You have array of integers. Write a recursive solution to determine whether or not the array contains a specific value. 
+
+def include?(array, target) 
+  return false if array.length == 0 
+  return true if array.first == target 
+
+  include?(array[1..-1], target) 
+
+end 
+
+p include?([1, 3, 3, 9], 5)
+p include?([], 5)
+p include?([5], 5)
