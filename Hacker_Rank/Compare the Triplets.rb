47a39c5e10ac33bc095806ab69d85a3ccca2635c
elaraby elaidy <elarabyelaidy@gmail.com>
2021-05-26 17:09:51 +0200

    elrefa3y calling me

---
+++ Hacker_Rank/Compare the Triplets.rb
@@ -0,0 +1,69 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+def solve(a0, a1, a2, b0, b1, b2)
+  # setting up target variables 
+  aliceScores = [a0, a1, a2]
+  bobScores = [b0, b1, b2]
+
+  # output variables 
+  aliceFinal = 0 
+  bobFinal = 0
+
+  # each with index automatically starts at zero
+  # aliceScore is the current score for alice with each loop
+  aliceScores.each_with_index do |aliceScore, i|
+    if aliceScore > bobScores[i]
+      aliceFinal += 1 
+    elsif aliceScore < bobScores[i]
+      bobFinal += 1
+    end
+  end 
+
+  # returns the two scores separated by a string space
+  return aliceFinal, bobFinal
+end 
+
+def compareTriplets(a, b)
+    # Write your code here
+  result = [0, 0]
+  counter_a = 0
+  counter_b = 0
+
+  a.each_with_index do |_element, index|
+    if a[index] > b[index]
+      result[0] = counter_a += 1
+    elsif a[index] < b[index]
+      result[1] = counter_b += 1
+    elsif a[index] == b[index]
+      next
+    end
+  end
+  result 
+end
+                
+                
+    
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+a = gets.rstrip.split.map(&:to_i)
+
+b = gets.rstrip.split.map(&:to_i)
+
+result = compareTriplets a, b
+
+fptr.write result.join " "
+fptr.write "\n"
+
+fptr.close()
