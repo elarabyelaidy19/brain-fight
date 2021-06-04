61ea7cce3a2102ac9325705b65b82cdb50e98c5f
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-04 17:17:33 +0200

    staircase

--- Hacker_Rank/Staircase.rb
+++ Hacker_Rank/Staircase.rb
@@ -1,2 +1,29 @@
 https://www.hackerrank.com/challenges/staircase/problem 
 
+
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n)
+    # Write your code here
+    i = 1
+    while i <= n 
+        spacecount = n - i 
+        hashcount = i 
+        puts " " * spacecount + "#" * hashcount
+        i += 1 
+    end 
+
+end
+
+n = gets.strip.to_i
+
+staircase n
