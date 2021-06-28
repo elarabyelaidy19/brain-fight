37ee0958e9cef17a0c3ee3baf6e8c0cfdac8ac47
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-28 14:41:23 +0200

    backTOback

--- Hacker_Rank/candles count.rb
+++ Hacker_Rank/candles count.rb
@@ -1,2 +1,33 @@
 https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=next-challenge&h_v=zen 
 
+
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles)
+    # Write your code here
+    maxes = candles.max
+    maxes_count = candles.count(maxes)
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+candles_count = gets.strip.to_i
+
+candles = gets.rstrip.split.map(&:to_i)
+
+result = birthdayCakeCandles candles
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
