9af921f5e5637fe91b4d70e21163a853bd321064
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-28 14:26:26 +0200

    backTOback

--- Hacker_Rank/min max sum.rb
+++ Hacker_Rank/min max sum.rb
@@ -1,1 +1,27 @@
 https://www.hackerrank.com/challenges/mini-max-sum/problem 
+
+
+
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr)
+    # Write your code here
+    min = arr.min 
+    max = arr.max 
+    sum = arr.sum 
+    print sum - max
+    print " "
+    print sum - min
+end 
+arr = gets.rstrip.split.map(&:to_i)
+
+miniMaxSum arr
