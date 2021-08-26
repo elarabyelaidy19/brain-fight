ea63c1057bb5d418f8ba8ed52d3efa27d97a4008
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-27 01:27:07 +0200

    Shape Area

--- CodeSignal/shapeArea.rb
+++ CodeSignal/shapeArea.rb
@@ -1,1 +1,5 @@
-# https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ
+# https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ 
+
+def shapeArea(n) 
+  area = (n*n) + ( (n-1) * (n-1) ) 
+end 
