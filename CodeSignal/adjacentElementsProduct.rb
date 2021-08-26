acf4efa4096d9b6cb65615a004f68bbc9f06231b
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-27 01:08:16 +0200

    max adjacent product

--- CodeSignal/adjacentElementsProduct.rb
+++ CodeSignal/adjacentElementsProduct.rb
@@ -1,3 +1,18 @@
 
 # https://app.codesignal.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m 
 
+def adjacentElementProduct(array) 
+  max = array[0] * array[1] 
+  last = array.length - 1 
+  i = 0 
+
+  while last > i  
+    if (array[i] * array[i+1]) > max 
+      max = (array[i] * array[i+1])  
+    end
+    i += 1 
+  end
+  
+  return max  
+end 
+
