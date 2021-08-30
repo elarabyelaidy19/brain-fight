9904bf07c46d378a256c63aaff4ad04829d99dd1
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-30 07:07:00 +0200

    java last word length

---
+++ Leetcode/Java/LastWord.java
@@ -0,0 +1,17 @@
+package Java;
+class lastWord{  
+  public int LasrWordLength(String s) { 
+    int lastWordLen = 0; 
+    int currentWordLen = 0; 
+
+    for(int i = 0; i < s.length(); i++) { 
+      if (s.charAt(i) == ' ') 
+        currentWordLen = 0; 
+      else  
+        currentWordLen++; 
+        lastWordLen = currentWordLen; 
+    }
+    return lastWordLen;
+  }
+
+}
