b8cc7eb3a21a98e38c218fb04ad7c3594c57feb4
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-10 17:14:43 +0200

    third max ruby

--- Leetcode/Ruby/third_maximum_number.rb
+++ Leetcode/Ruby/third_maximum_number.rb
@@ -1,2 +1,30 @@
 # https://leetcode.com/problems/third-maximum-number/ 
 
+# @param {Integer[]} nums
+# @return {Integer}
+def third_max(nums) 
+  fm = nil
+  sm = nil   
+  tm = nil 
+
+  nums.each do |num|
+      if (fm == nil || num > fm) 
+          tm, sm, fm = sm, fm, num 
+      elsif num == fm 
+          next
+      elsif (sm == nil || num > sm) 
+          tm, sm = sm, num
+      elsif num == sm 
+          next
+      elsif (tm == nil || num > tm) 
+          tm = num 
+      end 
+  end 
+    
+  if tm != nil 
+      tm 
+  else 
+      fm 
+  end 
+  
+end
