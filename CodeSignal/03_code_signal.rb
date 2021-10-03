d1eefb0837bed4ee0c76b12de76d86d24c36550c
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-03 19:25:52 +0200

    find email domain

--- CodeSignal/03_code_signal.rb
+++ CodeSignal/03_code_signal.rb
@@ -151,6 +151,28 @@ end
 end 
 ########################################################
 ########################################################
+=begin 
+https://app.codesignal.com/arcade/intro/level-10/TXFLopNcCNbJLQivP/solutions
+
+An email address such as "John.Smith@example.com" is made up of a local part 
+("John.Smith"), an "@" symbol, then a domain part ("example.com").
+
+The domain name part of an email address may only consist of letters, digits, 
+hyphens and dots. The local part, however, also allows a lot of different special characters. 
+Here you can look at several examples of correct and incorrect email addresses.
+
+Given a valid email address, find its domain part. 
+=end 
+
+def findEmailDomain(address)
+  at = /@/ 
+  i = 0 
+  address.map.each_cahr.with_index {|c, idx| i = idx if at.match(c) } 
+  address[i+1..-1]
+end
+
+# two  
+
 
 ########################################################
 ########################################################
