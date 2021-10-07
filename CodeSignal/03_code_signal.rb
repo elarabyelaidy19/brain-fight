bbafecc8db988ae6a6f464cb2bc331b68ad7cbc7
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-07 14:45:35 +0200

    line encoding

--- CodeSignal/03_code_signal.rb
+++ CodeSignal/03_code_signal.rb
@@ -298,6 +298,39 @@ end
 
 ########################################################
 ########################################################
+=begin 
+https://app.codesignal.com/arcade/intro/level-11/o2uq6eTuvk7atGadR 
+Given a string, return its encoding defined as follows:
+
+First, the string is divided into the least possible number of disjoint substrings 
+consisting of identical characters
+for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+Next, each substring with length greater than one is replaced with a concatenation 
+of its length and the repeating character
+for example, substring "bbb" is replaced by "3b"
+Finally, all the new strings are concatenated together in the same order and a new string is returned.
+=end
+def lineEncoding(s)
+  # increase size by elemnt to iterate upon array
+  s += " "
+  count = 1
+  len = s.length - 1 
+  new_s = []
+  for i in (1..len) 
+      # if previous element == next so we have two count = 1 and count += 1
+      if s[i] == s[i-1] 
+          count += 1 
+      else 
+          #
+          if count > 1 
+             new_s.append(count) 
+          end 
+          new_s.append(s[i-1]) 
+          count = 1
+      end 
+  end
+  new_s.join
+end
 
 ########################################################
 ########################################################
