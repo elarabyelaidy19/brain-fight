4985700b3828232198c21ac1998825950419da7a
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-13 02:51:10 +0200

    message encoding

--- CodeSignal/03_code_signal.rb
+++ CodeSignal/03_code_signal.rb
@@ -669,3 +669,20 @@ def filenames(names)
   end 
   res 
 end 
+
+
+########################################################
+########################################################
+=begin 
+https://app.codesignal.com/arcade/intro/level-12/sCpwzJCyBy2tDSxKW 
+For code = "010010000110010101101100011011000110111100100001", the output should be
+messageFromBinaryCode(code) = "Hello!".
+
+The first 8 characters of the code are 01001000, which is 72 in the binary numeral system.
+72 stands for H in the ASCII-table, so the first letter is H.
+Other letters can be obtained in the same manner. 
+=end 
+
+def messageFromBinaryCode(code)
+  code.chars.each_slice(8).to_a.map{|l| l.join("").to_i(2).chr}.join("")
+end
