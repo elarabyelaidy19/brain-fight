4b76ddde8e37f18c209ce2715d491ff15ca8493e
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-18 23:15:45 +0200

    all longest string

--- CodeSignal/ruby/array.rb
+++ CodeSignal/ruby/array.rb
@@ -108,3 +108,21 @@ def adjacentElementProduct(array)
   array.each_cons(2).map { |a, b| a * b }.max 
 end 
 
+
+
+#####################################################################
+#####################################################################
+
+# https://app.codesignal.com/arcade/intro/level-3/fzsCQGYbxaEcTr2bL/solutions?solutionId=xBd7NyiZJ4pH2kvc5 
+
+def all_longest_strings(arr) 
+  sorted_by_length = arr.map(&:length).max 
+  longest = []
+  arr.each do |str| 
+    if str.length == sorted_by_length 
+      longest << str
+    end 
+  end 
+
+  longest 
+end 
