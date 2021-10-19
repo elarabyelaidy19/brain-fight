2b0b3841cfc896768dfb65a083fbff0ed7122550
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-19 22:23:10 +0200

    alternating sum

--- CodeSignal/ruby/01_code_signal.rb
+++ CodeSignal/ruby/01_code_signal.rb
@@ -4,25 +4,6 @@
 #####################################################################
 #####################################################################
 
-# https://app.codesignal.com/arcade/intro/level-4/cC5QuL9fqvZjXJsW9 
-
-def alternating_sum(array) 
-  team_one = 0 
-  team_two = 0 
-  new_array = [] 
-
-  array.each_with_index do |idx| 
-    team_one += array[idx] if idx.even? 
-    team_two += array[idx] if idx.odd? 
-  end 
-
-  new_array << team_one << team_two 
-end 
-
-
-#####################################################################
-#####################################################################
-
 # https://app.codesignal.com/arcade/intro/level-4/xYXfzQmnhBvEKJwXP 
 
 def are_similar(a, b) 
