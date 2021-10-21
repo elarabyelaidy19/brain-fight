b3ccf2a970a893f23b251b3fad0423ba27420961
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-21 16:07:59 +0200

    is lucky

--- CodeSignal/java/Numbers.java
+++ CodeSignal/java/Numbers.java
@@ -25,5 +25,39 @@ class Numbers {
         return true;
 
     }    
+    /* 
+    https:// app.codesignal.com/arcade/intro/level-3/3AdBC97QNuhF6RwsQ
+    
+    A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+    Given a ticket number n, determine if it's lucky or not. */
+    boolean isLucky(int n) {
+        String number = Integer.toString(n);
+
+        int leftHalf = 0;
+        int rightHalf = 0;
+        int len = number.length();
+
+        for (int i = 0; i < len; i++) {
+            if (i < len / 2) {
+                leftHalf += number.charAt(i);
+            } else {
+                rightHalf += number.charAt(i);
+            }
+        }
+
+        return leftHalf == rightHalf;
+    }
+
+    boolean isLucky2(int n) {
+        String number = n + "";
+
+        int sum = 0;
+
+        for (int i = 0; i < number.length() / 2; i++) {
+            sum += (number.charAt(i) - number.charAt(number.length() - 1 - i));
+        }
+
+        return sum == 0;
+    }
 
 }
