fe76a8696f6c735114c1907fdf05e035718a4c7b
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-23 19:11:32 +0200

    max adjacent diff

--- CodeSignal/ruby/array.rb
+++ CodeSignal/ruby/array.rb
@@ -259,3 +259,31 @@ def makeArrayConsecutive2(s)
   return range - s.length+1 
 end 
 
+
+#####################################################################
+#####################################################################
+
+# https://app.codesignal.com/arcade/intro/level-5/EEJxjQ7oo7C5wAGjE
+
+# Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+## Example
+# For inputArray = [2, 4, 1, 0], the output should be
+# arrayMaximalAdjacentDifference(inputArray) = 3.# 
+
+def arrayMaximalAdjacentDifference(array) 
+  array.each_cons(2).map { |a, b| (a - b).abs }.max 
+end  
+
+# second 
+
+def arrayMaximalAdjacentDifference(array) 
+  max = 0 
+
+  for i in (1...array.length) 
+    diff = array[i] - array[i-1] 
+    diff = diff.abs 
+    max = diff if diff > max 
+  end 
+  max  
+end 
