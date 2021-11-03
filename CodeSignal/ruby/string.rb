5412bfbe4cb18531413f6d9382e01198101aa6eb
elaraby elaidy <elarabyelaidy@gmail.com>
2021-11-03 20:48:00 +0200

    rearrange permutation

--- CodeSignal/ruby/string.rb
+++ CodeSignal/ruby/string.rb
@@ -164,3 +164,33 @@ def add_border(picture)
   rect.push(stars)  
   rect 
 end 
+
+
+
+########################################################
+########################################################
+
+=begin Given an array of equal-length strings, you'd like to know if it's possible 
+to rearrange the order of the elements in such a way that each consecutive pair of strings differ by exactly one character. 
+Return true if it's possible, and false if not.
+Note: You're only rearranging the order of the strings, not the order of the letters within the strings!
+=end
+
+def stringsRearrangement(inputArray)
+  arr = inputArray.map(&:chars) 
+  arr = arr.permutation.to_a 
+  
+  arr.each do |a| 
+      return true if a.each_cons(2).all? { |x| diffrence(x[0], x[1]) == 1 }
+  end
+  false 
+end 
+
+def diffrence(s1, s2) 
+  count = 0
+  
+  s1.each_with_index do |c, i| 
+      count += 1 if c != s2[i] 
+  end 
+  count 
+end 
