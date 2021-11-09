9765c64865928857fb6ccedddf75cd76e04b1892
elaraby elaidy <elarabyelaidy@gmail.com>
2021-11-09 14:50:34 +0200

    knapsack lite

--- CodeSignal/java/Logical.java
+++ CodeSignal/java/Logical.java
@@ -11,5 +11,21 @@ public class Logical {
                 || (yourRight == friendsLeft && friendsRight == yourLeft));
 
     }
+
+    // https://app.codesignal.com/arcade/intro/level-9/r9azLYp2BDZPyzaG2
+    int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+        int sumValue = value1 + value2;
+        int sumWeight = weight1 + weight2;
+        int maxValue = Math.max(value1, value2);
+        if (weight1 > maxW && weight2 > maxW)
+            return 0;
+        if (maxW >= sumWeight)
+            return sumValue;
+        if (weight2 > maxW)
+            return value1;
+        if (weight1 > maxW)
+            return value2;
+        return maxValue;
+    }
     
 }
--- CodeSignal/ruby/logic.rb
+++ CodeSignal/ruby/logic.rb
@@ -28,3 +28,33 @@ def are_equally_strong(yourLeft, yourRight, friendsLeft, friendsRight)
   me.sort == my_friend.sort 
 end 
 
+
+
+
+#######################################################################
+#######################################################################
+# https://app.codesignal.com/arcade/intro/level-9/r9azLYp2BDZPyzaG2 
+
+def knapsackLight(value1, weight1, value2, weight2, maxW)
+  return (value1 + value2) if maxW >= (weight1 + weight2)  #
+  return 0 if maxW < weight1 && maxW < weight2 
+  return [value1, value2].max if weight1 <= maxW && weight2 <= maxW
+  return value1 if weight2 > maxW && weight1 <= maxW 
+  return value2 if weight1 > maxW && weight2 <= maxW 
+end
+
+# Two 
+def knapsackLight(value1, weight1, value2, weight2, maxW)
+  max_value = 0 
+  return value1 + value2 if (weight1 + weight2) <= maxW
+  
+  if value1 > value2 || weight2 > maxW 
+    max_value = value1 if maxW >= weight1 
+  else 
+    max_value = value2 if maxW >= weight2 
+  end 
+  max_value
+end 
+
+end
+
