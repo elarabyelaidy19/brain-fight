efadf66e7fd74b627fc3e4dd6e49351a6ff5de5f
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-03-09 17:38:52 +0200

    roundeness

--- CodeSignal/ruby/maths.rb
+++ CodeSignal/ruby/maths.rb
@@ -278,7 +278,27 @@ def solution(k)
     result 
 end
 
+=begin 
+We want to turn the given integer into a number that has only one non-zero digit using a tail rounding approach. 
+This means that at each step we take the last non 0 digit of the number and round it to 0 or to 10. 
+If it's less than 5 we round it to 0 if it's larger than or equal to 5 we round it to 10 
+(rounding to 10 means increasing the next significant digit by 1). 
+The process stops immediately once there is only one non-zero digit lef 
+1445 -> 1450 -> 1500 -> 2000. 
+1234 -> 1230 -> 1200 -> 1000. 
+n = 15, the output should be
+solution(n) = 20
+=end 
 
+
+def solution(n)
+    dot = 1 
+    while n > 10 
+        dot *= 10 
+        n = (n/10) + ((n % 10 < 5) ? 0 : 1) 
+    end 
+    dot * n
+end
 
 
 
